b216098084e850fdbf56d200cb639e7bce4b5636
Kennedy.Juma <kennedy.juma@sgnewtech.com>
2024-01-27 18:44:30 +0300

    initial commit

---
+++ ASP.NET Core/EntityFrameworkCore/WikiCode/WikiCode.DataAccessor/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikiCode.Models.Models;
+
+namespace WikiCode.DataAccessor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer();
+        }
+    }
+}
